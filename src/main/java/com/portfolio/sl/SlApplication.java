0f93ec8792bb76fd3a41a7d366313cbb15565772
santiago-lange <santiagolange2504@gmail.com>
2023-05-21 20:07:07 -0300

    instalacion spring boot

---
+++ src/main/java/com/portfolio/sl/SlApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.sl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/sl/SlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.sl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
